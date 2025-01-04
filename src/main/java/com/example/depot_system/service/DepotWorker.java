f1e6124298a22dab000377cabbaf1278145ac6db
Ahamad Falah <mnfalahahamad@gmail.com>
2025-01-04 02:26:39 +0000

    feat: Initialize DepotWorker class

---
+++ src/main/java/com/example/depot_system/service/DepotWorker.java
@@ -0,0 +1,23 @@
+package com.example.depot_system.service;
+
+import com.example.depot_system.model.Customer;
+import com.example.depot_system.model.Parcel;
+
+public class DepotWorker {
+    private String name;
+    private Customer currentCustomer;
+    private Parcel currentParcel;
+
+    public DepotWorker(String name) {
+        this.name = name;
+    }
+
+//    public void processCustomer(Customer customer, Parcel parcel) {
+//    }
+
+//    public double calculateFee(Parcel parcel) {
+//    }
+
+//    public void releaseParcel() {
+//    }
+}
